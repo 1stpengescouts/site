ac1ae85758727b6fe72661ae85718393fff4d1e6
Frankie Wittevrongel <frankie@cupoftea.io>
2023-06-11 22:58:32 +0100

    env fix

--- app.php
+++ app.php
@@ -1,53 +1,13 @@
 <?php
 
+use App\Env;
+
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createMutable(__DIR__);
+$dotenv = Dotenv\Dotenv::create(Env::getRepository(),__DIR__);
 $dotenv->load();
 $dotenv->required([
     'DB_NAME',
     'DB_USER',
     'DB_PASS',
 ]);
-
-if (!function_exists('env')) {
-    /**
-     * Gets the value of an environment variable. Supports boolean, empty and null.
-     *
-     * @param  string  $key
-     * @param  mixed   $default
-     * @return mixed
-     */
-    function env($key, $default = null)
-    {
-        $value = getenv($key);
-
-        if ($value === false) {
-            return $default instanceof Closure ? $default() : $default;
-        }
-
-        switch (strtolower($value)) {
-            case 'true':
-            case '(true)':
-                return true;
-
-            case 'false':
-            case '(false)':
-                return false;
-
-            case 'empty':
-            case '(empty)':
-                return '';
-
-            case 'null':
-            case '(null)':
-                return;
-        }
-
-        if (preg_match('/\A([\'"])(.*)\1\z/', $value, $matches)) {
-            return $matches[2];
-        }
-
-        return $value;
-    }
-}
---
+++ app/helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Env;
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        return Env::get($key, $default);
+    }
+}
+
+if (! function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @param  mixed  ...$args
+     * @return mixed
+     */
+    function value($value, ...$args)
+    {
+        return $value instanceof Closure ? $value(...$args) : $value;
+    }
+}
