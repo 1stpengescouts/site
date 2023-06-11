502e6cfd325485a7c2b83966bb3fe4d1fa5e4c26
Frankie Wittevrongel <frankie@cupoftea.io>
2023-06-11 20:00:27 +0100

    init

---
+++ wp/wp-config.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once dirname(__DIR__) . '/config.php';
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
