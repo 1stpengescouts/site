aefd0583527b3e91b08d7bfb1476d273a467fcb3
Frankie Wittevrongel <frankie@cupoftea.io>
2023-06-16 19:54:29 +0100

    Create child theme

---
+++ public/content/themes/1stpenge/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function bs_1stpenge_block_assets() {
+    wp_enqueue_style('bs-1stpenge-theme',
+        get_template_directory_uri() . '/assets/css/app.css',
+        ['theme'],
+        wp_get_theme()->get('Version')
+    );
+}
+
+add_action('enqueue_block_assets', 'bs_1stpenge_block_assets', 20);
