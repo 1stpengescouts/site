3577bc1b787f514e1d8baa7da3208865826ebd4a
Frankie Wittevrongel <frankie@cupoftea.io>
2023-06-19 17:07:36 +0100

    Typography fixes

--- public/content/themes/1stpenge/functions.php
+++ public/content/themes/1stpenge/functions.php
@@ -1,9 +1,21 @@
 <?php
 
+function bs_1stpenge_preconnect() {
+    echo '<link rel="preconnect" href="https://fonts.googleapis.com">' . PHP_EOL;
+    echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' . PHP_EOL;
+}
+
+add_action('wp_head', 'bs_1stpenge_preconnect', 2);
+
 function bs_1stpenge_block_assets() {
-    wp_enqueue_style('bs-1stpenge-theme',
+    wp_enqueue_style(
+        'Nunito+Sans',
+        'https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap'
+    );
+    wp_enqueue_style(
+        'bs-1stpenge-theme',
         get_stylesheet_directory_uri() . '/assets/css/app.css',
-        ['theme'],
+        ['Nunito+Sans', 'theme'],
         wp_get_theme()->get('Version')
     );
 }
