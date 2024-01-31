9dc40f9e204b710348e52cedc55431dc3c11c4a5
Frankie Wittevrongel <frankie@cupoftea.io>
2024-01-31 15:23:24 +0000

    Moving wp-config up to prevent WP update issues

--- public/wp-config.php
+++ public/wp-config.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once dirname(__DIR__, 2) . '/config.php';
+require_once dirname(__DIR__) . '/config.php';
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+	define( 'ABSPATH', __DIR__ . '/wp/');
 }
 
 /** Sets up WordPress vars and included files. */
